f008163f0123f47ce46d0210a08a4bb634c583dc
samuel guimarães <86096715+samuelguim@users.noreply.github.com>
2024-12-07 06:53:14 +0000

    modified:   ingredientes_tipicos 	modified:   ingredientes_tipicos.c

--- PrimeiraSemana/ingredientes_tipicos.c
+++ PrimeiraSemana/ingredientes_tipicos.c
@@ -3,6 +3,35 @@
 #include <stdbool.h>
 
 
+bool confereTipico (int i, int n, int m, int quantIngr, char ingredientes[quantIngr][m][50], char porcoesTipicas[50][m], char tipicos2[n][50]){
+    int contador = 0;
+    for(int k = 0; k < quantIngr; k++){
+        for (int j = 0; j < n; j++){
+            if (strcmp(ingredientes[i][k], tipicos2[j]) == 0){
+                contador++;
+            }
+        }
+    }
+    
+    // conta quantos ingredientes são porções típicas
+    for(int k = 0; k < quantIngr; k++){
+        for (int j = 0; j < m; j++){
+            if (strcmp(ingredientes[i][k], porcoesTipicas[j]) == 0){
+                contador++;
+            }
+        }
+    }
+    if(quantIngr != 0){
+        if (contador > quantIngr/2){
+        return true;
+    }
+    }
+    else {
+        return false;
+    }
+}
+
+
 int main(){
 
     //lê os ingredientes típicos
@@ -24,15 +53,17 @@ int main(){
     }
 
     scanf("%i", &m);
+    char tiposDePorcao[m][13];
+    char porcoesTipicas[50][m];
 
     for (int i = 0; i < m; i++){
         char nomePorcao[50];
         int quantIngr = 0;
         scanf("%s %i", nomePorcao, &quantIngr);
 
-        char ingredientes_temp[quantIngr*50];
-        char ingredientes [quantIngr][50];
-        scanf(" %[^\n]s", ingredientes_temp);
+        char ingredientes_temp[m][quantIngr*50];
+        char ingredientes[m][quantIngr][50];
+        scanf(" %[^\n]s", ingredientes_temp[i]);
         int j = 0;
 
         //evita bug array corrompido
@@ -42,42 +73,29 @@ int main(){
         }
 
 
-        for (int i = 0; i < quantIngr; i++){
+        for (int l = 0; l < quantIngr; l++){
             int k = 0;
-            while(ingredientes_temp[j] != ' ' && j < n*50){
-                ingredientes[i][k] = ingredientes_temp[j];
+            while(ingredientes_temp[i][j] != ' ' && j < n*50){
+                ingredientes[i][l][k] = ingredientes_temp[i][j];
                 j++;
                 k++;
             }
             j++;
         }
         
-        // conta quantos ingredientes são típicos
-        int contador = 0;
-        for(int i = 0; i < quantIngr; i++){
-            for (int j = 0; j < n; j++){
-                if (strcmp(ingredientes[i], tipicos2[j]) == 0){
-                    contador++;
-                }
-            }
-        }
-
-        // conta quantos ingredientes são porções típicas
-        char porcoesTipicas[50][m];
-        for(int i = 0; i < m; i++){
-            for (int j = 0; j < n; j++){
-                if (strcmp(ingredientes[i], tipicos2[j]) == 0){
-                    contador++;
-                }
-            }
-        }
-        
-        // define se a porção é típica ou comum
-        if (contador > quantIngr/2){
-            printf("porcao tipica\n");
+        if (confereTipico(i, n, m, quantIngr, ingredientes, porcoesTipicas, tipicos2)){
+            strcpy(tiposDePorcao[i],"porcao tipica");
+            //printf("porcao tipica\n");
         }
         else {
-            printf("porcao comum\n");
+            strcpy(tiposDePorcao[i],"porcao comum");
+            //printf("porcao comum\n");
+        }
+
+        if (i == m-1){
+            for (int p = 0; p < m; p++){
+                printf("%s\n", tiposDePorcao[p]);
+            }
         }
     }
     
