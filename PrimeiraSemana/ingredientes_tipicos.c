c683b260789a835eddf240b093101a3edf28d726
samuel guimarães <86096715+samuelguim@users.noreply.github.com>
2024-12-07 06:53:00 +0000

    modified:   .vscode/settings.json 	modified:   ingredientes_tipicos 	modified:   ingredientes_tipicos.c 	new file:   teste 	new file:   teste.c 	modified:   ingredientes_tipicos 	modified:   ingredientes_tipicos.c

--- PrimeiraSemana/ingredientes_tipicos.c
+++ PrimeiraSemana/ingredientes_tipicos.c
@@ -3,21 +3,6 @@
 #include <stdbool.h>
 
 
-/*
-void stringParaArray(char **array, int tamanho){
-    int j = 0;
-    for (int i = 0; i < tamanho; i++){
-        int k = 0;
-        while(*array[j] != ' '){
-            array[i][k] = *array[j];
-            j++;
-            k++;
-        }
-        j++;
-    }
-}
-*/
-
 int main(){
 
     //lê os ingredientes típicos
@@ -25,9 +10,8 @@ int main(){
     scanf("%i", &n);
     char tipicos_temp[n*50];
     scanf(" %[^\n]s", tipicos_temp);
+    
     char tipicos[n][50];
-
-    //separa os ingredientes em um array
     int j = 0;
     for (int i = 0; i < n; i++){
         int k = 0;
@@ -39,18 +23,25 @@ int main(){
         j++;
     }
 
-
     scanf("%i", &m);
-    char nomePorcao[50];
-    int quantIngr = 0;
 
     for (int i = 0; i < m; i++){
-        
+        char nomePorcao[50];
+        int quantIngr = 0;
         scanf("%s %i", nomePorcao, &quantIngr);
+
         char ingredientes_temp[quantIngr*50];
         char ingredientes [quantIngr][50];
         scanf(" %[^\n]s", ingredientes_temp);
         int j = 0;
+
+        //evita bug array corrompido
+        char tipicos2[n][50];
+        for (int i = 0; i < n; i++){
+            strcpy(tipicos2[i], tipicos[i]);
+        }
+
+
         for (int i = 0; i < quantIngr; i++){
             int k = 0;
             while(ingredientes_temp[j] != ' ' && j < n*50){
@@ -65,7 +56,7 @@ int main(){
         int contador = 0;
         for(int i = 0; i < quantIngr; i++){
             for (int j = 0; j < n; j++){
-                if (strcmp(ingredientes[i], tipicos[j]) == 0){
+                if (strcmp(ingredientes[i], tipicos2[j]) == 0){
                     contador++;
                 }
             }
@@ -75,15 +66,15 @@ int main(){
         char porcoesTipicas[50][m];
         for(int i = 0; i < m; i++){
             for (int j = 0; j < n; j++){
-                if (strcmp(ingredientes[i], tipicos[j]) == 0){
+                if (strcmp(ingredientes[i], tipicos2[j]) == 0){
                     contador++;
                 }
             }
         }
         
+        // define se a porção é típica ou comum
         if (contador > quantIngr/2){
             printf("porcao tipica\n");
-
         }
         else {
             printf("porcao comum\n");
