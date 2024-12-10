2fb6cf979cab7e04bd43c47b2cdaa3fe995f558c
Caio <caiaoviniciuscc@gmail.com>
2024-12-10 16:54:32 -0300

    add problema 1609

---
+++ SegundaSemana/carneirinhos.py
@@ -0,0 +1,25 @@
+# 1609 - Contando Carneirinhos
+# TLE
+#16:30
+
+# f = open('SegundaSemana/teste.txt', )
+
+import sys
+
+def encontra_unicos(lista):
+    return list(set(lista))
+
+def main():
+    resultado = []
+    n = int(sys.stdin.readline())
+    for _ in range(n):
+        qtd_carneirinhos = sys.stdin.readline()
+        carneirinhos = sys.stdin.readline().split()
+        carneirinhos_distintos = len(encontra_unicos(carneirinhos))
+        resultado.append(carneirinhos_distintos)
+
+
+    print('\n'.join(map(str,resultado)))
+
+if __name__ == '__main__':
+    main()
