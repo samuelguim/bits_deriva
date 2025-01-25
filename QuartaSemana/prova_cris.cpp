781ab693357d2e8ecaeeff6ca05509e6ed9d9e66
samuel guimarães <86096715+samuelguim@users.noreply.github.com>
2025-01-25 02:32:01 +0000

    modified:   .vscode/tasks.json 	modified:   QuartaSemana/prova_cris 	modified:   QuartaSemana/prova_cris.cpp
    Changes not staged for commit:
    	modified:   QuartaSemana/prova_cris
    	modified:   QuartaSemana/prova_cris.cpp

--- QuartaSemana/prova_cris.cpp
+++ QuartaSemana/prova_cris.cpp
@@ -39,6 +39,12 @@ int posicao_diferenca_string(string a, string b){
     return diferenca;
 }
 
+void troca_simples(string palavras[], int a, int b){
+    string temp = palavras[a];
+    palavras[a] = palavras[b];
+    palavras[b] = temp;
+}
+
 void trocar_palavras(string palavras[], int a, int b){
     for (int i = a; i < b; i++){
         int prox = i + 1;
@@ -46,59 +52,59 @@ void trocar_palavras(string palavras[], int a, int b){
     }
 }
 
-void troca_simples(string palavras[], int a, int b){
-    string temp = palavras[a];
-    palavras[a] = palavras[b];
-    palavras[b] = temp;
-}
-
 int main(){
     int n, k;
     int instancia = 0;
     string str_instancia;
     vector<string> resultadofinal;
-
-    do{
+    
+    while (true) {
         cin >> n >> k;
+        if (n == 0 && k == 0){
+            break;
+        }
+
         string alunos[n];
 
         for(int i = 0; i < n; i++){
             cin >> alunos[i];
         }
 
-        str_instancia = "Instância " + to_string(instancia + 1);
+        str_instancia = "Instancia " + to_string(instancia + 1);
         resultadofinal.push_back(str_instancia);
         resultadofinal[instancia] = resultadofinal[instancia] + "\n";
 
-        for(int j = 0; j < k; j++){
-            //busca a maior diferença na ordem lexicográfica
-            int menor_posicao = 20;
-            int maior_diferenca = 0;
-            int posicao_a_trocar = 0;
-            int x;
+        //busca a maior diferença na ordem lexicográfica
+        int menor_posicao = 20;
+        int maior_diferenca = 0;
+        int posicao_a_trocar = 0;
+        int trocas_realizadas = 0;
 
-            for(int i = 0; i < n - 1; i++){
-                int proximo = i + 1;
-                x = alunos[i].compare(alunos[proximo]);
-                //cout << x; 
-                if (posicao_diferenca_string(alunos[i], alunos[proximo]) <  menor_posicao && posicao_diferenca_string(alunos[i], alunos[proximo]) !=0){
-                    if (alunos[i].compare(alunos[proximo]) > maior_diferenca){
-                        menor_posicao = posicao_diferenca_string(alunos[i], alunos[proximo]);
-                        maior_diferenca = alunos[i].compare(alunos[proximo]);
-                        posicao_a_trocar = i;
+        for(int j = 0; j < n; j++){
+            for(int i = 1; i <= k; i++){
+                int proximo = j + i;
+                if(sizeof(alunos) < proximo){
+                    if (posicao_diferenca_string(alunos[j], alunos[proximo]) <  menor_posicao && posicao_diferenca_string(alunos[j], alunos[proximo]) !=0){
+                        if (alunos[j].compare(alunos[proximo]) > maior_diferenca){
+                            menor_posicao = posicao_diferenca_string(alunos[j], alunos[proximo]);
+                            maior_diferenca = alunos[j].compare(alunos[proximo]);
+                            posicao_a_trocar = i;
+                        }
                     }
                 }
             }
-            trocar_palavras(alunos, posicao_a_trocar, posicao_a_trocar++);
+            int prox = posicao_a_trocar + 1;
+            trocar_palavras(alunos, posicao_a_trocar, prox);
         }
 
         //imprime o resultado
         for(int i = 0; i < n; i++){
             resultadofinal[instancia] = resultadofinal[instancia] + alunos[i] + " ";
         }
+    
     resultadofinal[instancia] = resultadofinal[instancia] + "\n\n";
     instancia++;
-    } while (n != 0 || k != 0);
+    }
 
     for (int i = 0; i < resultadofinal.size(); i++){
         cout << resultadofinal[i];
