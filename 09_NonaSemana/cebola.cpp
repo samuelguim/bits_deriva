72dc786197f035fe6409fe0685800c23b3afa9dd
Samuel <samuelguimaraaes@gmail.com>
2025-03-22 17:42:45 -0300

    modified:   09_NonaSemana/cebola.cpp 	modified:   09_NonaSemana/cebola.exe

--- 09_NonaSemana/cebola.cpp
+++ 09_NonaSemana/cebola.cpp
@@ -11,9 +11,8 @@ struct Ponto {
 };
 
 vector<Ponto> graham_scan(vector<Ponto> pontos) {
-    if (pontos.size() <= 3) return pontos; // casos triviais
+    if (pontos.size() <= 3) return pontos;
     
-    // encontra o ponto com menor coordenada y (e mais à esquerda em caso de empate)
     int idx_minimo = 0;
     for (int i = 1; i < pontos.size(); i++) {
         if (pontos[i].y < pontos[idx_minimo].y || 
@@ -22,49 +21,38 @@ vector<Ponto> graham_scan(vector<Ponto> pontos) {
         }
     }
     
-    // coloca o ponto inicial na primeira posição
     swap(pontos[0], pontos[idx_minimo]);
     Ponto ponto_inicial = pontos[0];
     
-    // ordena os pontos pelo ângulo polar em relação ao ponto_inicial
     sort(pontos.begin() + 1, pontos.end(), [&ponto_inicial](const Ponto& p1, const Ponto& p2) {
-        // calcula o produto vetorial para determinar qual ponto tem menor ângulo
         double prod = (p1.x - ponto_inicial.x) * (p2.y - ponto_inicial.y) - 
                       (p1.y - ponto_inicial.y) * (p2.x - ponto_inicial.x);
         
         if (prod == 0) {
-            // se colineares, escolhe o mais próximo primeiro
             double dist1 = pow(p1.x - ponto_inicial.x, 2) + pow(p1.y - ponto_inicial.y, 2);
             double dist2 = pow(p2.x - ponto_inicial.x, 2) + pow(p2.y - ponto_inicial.y, 2);
             return dist1 < dist2;
         }
         
-        return prod > 0; // ordenação anti-horária
+        return prod > 0;
     });
     
-    // função para determinar se temos uma curva à esquerda, direita ou colinear
     auto produto_vetorial = [](const Ponto& p1, const Ponto& p2, const Ponto& p3) -> double {
         return (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x);
     };
     
-    // inicializa a pilha com os primeiros pontos
     vector<Ponto> pilha;
     pilha.push_back(pontos[0]);
     pilha.push_back(pontos[1]);
     
-    // para cada ponto ordenado pelo ângulo
     for (int i = 2; i < pontos.size(); i++) {
-        // enquanto temos pelo menos 2 pontos na pilha e o último movimento não forma uma curva à esquerda
         while (pilha.size() >= 2 && produto_vetorial(pilha[pilha.size() - 2], pilha[pilha.size() - 1], pontos[i]) <= 0) {
-            // remove o último ponto da pilha
             pilha.pop_back();
         }
         
-        // adiciona o ponto atual à pilha
         pilha.push_back(pontos[i]);
     }
     
-    // a pilha final contém os pontos do fecho convexo
     return pilha;
 }
 
@@ -100,12 +88,13 @@ int main(){
             camada.clear();
             camada = graham_scan(pontos);
             remover_pontos(pontos, camada);
+            quantidade_camadas = quantidade_camadas + 1;
         }
         if (quantidade_camadas % 2 == 0){
-            cout << " Do not take this onion to the lab!";
+            cout << "Take this onion to the lab!!\n";
         }
         else{
-            cout << "Take this onion to the lab!";
+            cout << "Do not take this onion to the lab!\n";
         }
 
         
